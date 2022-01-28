9febc6e1b00b064c132e6ad8bf2b575d0fb467f4
Matt <mattbusby86@gmail.com>
2022-01-28 15:56:28 +0000

    pet model MVP

--- models/pet.py
+++ models/pet.py
@@ -1,3 +1,14 @@
-# pet class 
+class Pet:
 
-# name, dob, type of animal, owner, owner phone number, treatment notes (dated?).
+    def __init__(self, pet_name, dob, type_of_animal, owner_name, owner_ph, treatment_notes, id = None):
+        self.pet_name = pet_name
+        self.dob = dob
+        self.type_of_animal = type_of_animal
+        self.owner_name = owner_name
+        self.owner_ph = owner_ph
+        self.treatment_notes = treatment_notes
+        self.id = id
+
+    
+
+# owner as a seperate class (many to many?), treatment notes (dated?).
