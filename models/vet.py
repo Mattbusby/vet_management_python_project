239a77324fe346f81f73d535b85adb7a8ddfddff
Matt <mattbusby86@gmail.com>
2022-01-28 16:09:17 +0000

    vet model MVP

--- models/vet.py
+++ models/vet.py
@@ -1,3 +1,11 @@
-# vet class
+class Vet:
+    
+    def __init__(self, first_name, last_name, grad_date, fun_fact, id = None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.grad_date = grad_date
+        self.fun_fact = fun_fact
+        self.id = id
 
-# name, vet school grad date, fun fact.
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
