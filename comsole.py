0785e5793c4c7e8314d1fac7656778a13abcbf81
Matt <mattbusby86@gmail.com>
2022-01-28 15:50:03 +0000

    console outlined

--- comsole.py
+++ comsole.py
@@ -1,7 +1,35 @@
-# import 
+import pdb
+from models.pet import Pet
+from models.vet import Vet
 
-# repo's
+import repositories.pet_repository as pet_repository
+import repositories.vet_repository as vet_repository
 
-# vets n pets
+pet_repository.delete_all()
+vet_repository.delete_all()
+
+vet1 = Vet(   NAME  ETC    )
+vet_repository.save(vet1)
+vet2 = Vet(   NAME  ETC    )
+vet_repository.save(vet2)
+
+
+pet_1 = Pet(   NAME  ETC    , vet1)
+pet_repository.save(pet_1)
+
+pet_2 = Pet(   NAME  ETC    , vet1)
+pet_repository.save(pet_2)
+
+pet_3 = Pet(   NAME  ETC    , vet2)
+pet_repository.save(pet_3)
+
+Pet_4 = Pet(   NAME  ETC    , vet1)
+pet_repository.save(pet_1)
+
+pet_5 = Pet(   NAME  ETC    , vet1)
+pet_repository.save(pet_2)
+
+pet_6 = Pet(   NAME  ETC    , vet2)
+pet_repository.save(pet_3)
 
 pdb.set_trace()
