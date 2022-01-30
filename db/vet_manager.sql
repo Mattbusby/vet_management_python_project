07e4fa7c8c0f3f90f8eeba967a7ee292763eaefb
Matt <mattbusby86@gmail.com>
2022-01-30 21:32:12 +0000

    sql added

--- db/vet_manager.sql
+++ db/vet_manager.sql
@@ -1,3 +1,24 @@
--- drop pets before vets
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS vets;
 
--- leave room to add owners as a class later if pushing for many to many with pets becoming the link
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    grad_date VARCHAR(255),
+    fun_fact VARCHAR(255)
+);
+
+CREATE TABLE pets (
+    id SERIAL PRIMARY KEY,
+    pet_name VARCHAR(255),
+    dob VARCHAR(255),
+    type_of_animal VARCHAR(255),
+    owner_name VARCHAR(255),
+    owner_ph VARCHAR(255),
+    treatment_notes TEXT
+    vet_id INT REFERENCES vets(id)
+);
+
+
+-- add owners as a class later if pushing for many to many with pets becoming the link
